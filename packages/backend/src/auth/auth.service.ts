9d58e4a55fcea76126e256f1898123e7e53d8b16
Boluwatife Johnson <jaycewap@gmail.com>
2025-06-10 05:57:55 +0100

    feat: enhance AuthService with improved error handling and return structure; update type definitions

--- packages/backend/src/auth/auth.service.ts
+++ packages/backend/src/auth/auth.service.ts
@@ -1,29 +1,44 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
 
 import { AuthLoginDto } from './dto/create-auth.dto';
 import { findUserByEmail } from 'utils/helpers';
+import { Projects, Tasks, Users } from '@prisma/client';
+
+type AuthResponse<TUser extends Users = Users> = Promise<{
+  user: TUser & {
+    tasks: Tasks[];
+    watching: Tasks[];
+    projects: Projects[];
+    createdProjects: Projects[];
+  };
+  auth: {
+    token: string;
+    refresh: string;
+  };
+}>;
 
 @Injectable()
 export class AuthService {
-  constructor(private jwtServce: JwtService) { }
+  constructor(private jwtServce: JwtService) {}
 
-  async logUserIn(data: AuthLoginDto) {
+  async logUserIn(data: AuthLoginDto): AuthResponse {
     try {
       const user = await findUserByEmail(data.email);
 
       const compareHash = bcrypt.compareSync(data.password, user.passwordHash);
 
-      if (!compareHash)
-        return new HttpException(
-          'no user with this email was found',
-          HttpStatus.NOT_FOUND,
-        );
+      if (!compareHash) throw new NotFoundException('user not found')
 
-      return { user };
+      const auth = {
+        token: this.jwtServce.sign({ userId: user.id }),
+        refresh: this.jwtServce.sign({ userId: user.id }, { expiresIn: '7d' }),
+      };
+
+      return { user, auth };
     } catch (error: any) {
-      return new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new InternalServerErrorException(error);
     }
   }
 }
--- packages/shared/src/types/Auth.ts
+++ packages/shared/src/types/Auth.ts
@@ -1,14 +1,6 @@
-import { IsEmail, Min } from "class-validator"
-
 export interface ILoginData {
   email: string
   password: string
 }
 
-export class LoginDTO {
-  @IsEmail()
-  email!: string
-
-  @Min(6)
-  password!: string
-}
+export * from '../../../backend/types/auth'
